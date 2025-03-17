b37293ea6fdf54d984e5a368073a4355b1aa1d1e
danielemurgioni <dashomew@gmail.com>
2025-03-17 16:05:35 +0100

    [UPD] Prompt the user for age and kilometers to travel

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// permetto l'inserimento di due valori (età e chilometri percorso) da parte dell'utente
+let userKm = prompt("Inserisci il numero di chilometri che devi percorerre");
+let userAge = prompt("Inserisci la tua età")
+
+console.log(userKm, userAge)
