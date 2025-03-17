5d5112976074f7cececa22e4de3f2cd3eb2d793f
danielemurgioni <dashomew@gmail.com>
2025-03-17 17:00:32 +0100

    [UPD] Discount ticketCost when user have "x" age range

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 // permetto l'inserimento di due valori (età e chilometri percorso) da parte dell'utente
 let userKm = prompt("Inserisci il numero di chilometri che devi percorerre");
-let userAge = prompt("Inserisci la tua età")
+let userAge = prompt("Inserisci la tua età");
 
-console.log(userKm+"km "+"Age:"+userAge)
+console.log(userKm+"km "+"Age:"+userAge);
 
 // calcolo il costo del biglietto per chilometro
 
-let ticketCost = 0.21 * userKm
+let ticketCost = 0.21 * userKm;
 
-console.log(ticketCost)
+console.log(ticketCost);
+
+// se l'utente è minorenne o oltre i 65 anni avra uno sconto, altrimenti paga il costo totale del biglietto
+
+let underAge = 17;
+let overAge = 65;
+
+let ticketDiscount;
+
+if(userAge <= underAge){
+    ticketDiscount = ticketCost * 0.8;
+}
+else if(userAge > overAge){
+    ticketDiscount = ticketCost * 0.6;
+}
+else{
+    ticketDiscount = ticketCost;
+}
+
+console.log(ticketDiscount);
